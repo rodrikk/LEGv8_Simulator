356f02af453afd4cc9ab706418e326e095f8740b
rodrikk <rbauhern@gmail.com>
2025-02-02 11:36:09 +0100

    Initial spring commit

---
+++ simulator/src/main/java/com/legv8/simulator/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.legv8.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorApplication.class, args);
+	}
+
+}
---
+++ simulator/src/test/java/com/legv8/simulator/SimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legv8.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
